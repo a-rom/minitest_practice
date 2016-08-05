4652cca9595c3a4d112a16bc188e597fd68caf5a
a-rom <a1623at@aiit.ac.jp>
2016-08-05 19:41:04 +0000

    modify mini_test_practice.rb

--- lib/mini_test_practice.rb
+++ lib/mini_test_practice.rb
@@ -3,7 +3,7 @@ require "mini_test_practice/version"
 module MiniTestPractice
   class Main
     def odd?(n)
-      if (n % 3) == 1
+      if (n % 2) == 1
         return true
       else
         return false
